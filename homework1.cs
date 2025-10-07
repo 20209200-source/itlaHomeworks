8366b25d82604ef0271d5197677d21444c356095
20209200-source <20209200@itla.edu.do>
2025-10-07 18:00:26 -0400

    Adding the first homework

---
+++ homework1.cs
@@ -0,0 +1,54 @@
+﻿public class Autorepuesto
+{
+    public string Nombre { get; set; }
+    public string Codigo { get; set; }
+    public decimal Precio { get; set; }
+    public string Marca { get; set; }
+}
+
+
+public class RepuestoMecanico : Autorepuesto
+{
+    public string Material { get; set; }
+}
+public class Motor : RepuestoMecanico
+{
+    public int CaballosDeFuerza { get; set; }
+    public string TipoCombustible { get; set; }
+}
+
+
+public class Transmision : RepuestoMecanico
+{
+    public string Tipo { get; set; } 
+    public int Velocidades { get; set; }
+}
+
+public class RepuestoElectrico : Autorepuesto
+{
+    public int Voltaje { get; set; }
+}
+public class Bateria : RepuestoElectrico
+{
+    public int CapacidadAmperios { get; set; }
+}
+
+public class Alternador : RepuestoElectrico
+{
+    public int Amperaje { get; set; }
+}
+public class Accesorio : Autorepuesto
+{
+    public bool EsOriginal { get; set; }
+}
+public class Espejo : Accesorio
+{
+    public bool EsElectrico { get; set; }
+}
+public class Limpiaparabrisas : Accesorio
+{
+    public int TamañoPulgadas { get; set; }
+}
+
+
+
